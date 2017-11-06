01a640b8aedfd8dfc6338281e78c73a06b4bd1a6
GplCart <gplcart.software@gmail.com>
2017-11-06 17:51:01 +0200

    Core API changes

--- OmnipayLibrary.php
+++ OmnipayLibrary.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\modules\omnipay_library;
 
-use gplcart\core\Module;
+use gplcart\core\Module,
+    gplcart\core\Config;
 
 /**
  * Main class for Omnipay Library module
@@ -18,11 +19,11 @@ class OmnipayLibrary extends Module
 {
 
     /**
-     * Constructor
+     * @param Config $config
      */
-    public function __construct()
+    public function __construct(Config $config)
     {
-        parent::__construct();
+        parent::__construct($config);
     }
 
     /**
